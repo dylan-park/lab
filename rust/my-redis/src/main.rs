4adb7b789d7a7c0eb6495cd2e97ec7c2dc20908b
Dylan Park <dylan.park@att.net>
2026-04-16 15:51:05 -0500

    Added shared state to redis server

--- rust/my-redis/src/main.rs
+++ rust/my-redis/src/main.rs
@@ -1,28 +1,39 @@
+use bytes::Bytes;
 use mini_redis::{Connection, Frame};
+use std::{
+    collections::HashMap,
+    sync::{Arc, Mutex},
+};
 use tokio::net::{TcpListener, TcpStream};
+
+type Db = Arc<Mutex<HashMap<String, Bytes>>>;
 
 #[tokio::main]
 async fn main() {
     // bind the listener to the address
     let listener = TcpListener::bind("127.0.0.1:6379").await.unwrap();
 
+    println!("Listening");
+
+    let db = Arc::new(Mutex::new(HashMap::new()));
+
     loop {
         // the second item contains the ip and port of the new connection
         let (socket, _) = listener.accept().await.unwrap();
+        // clone the handle to the hash map.
+        let db = db.clone();
+
+        println!("Accepted");
         // a new task is spawned for each inbound socket. the socket is
         // moved to the new task and processed there
         tokio::spawn(async move {
-            process(socket).await;
+            process(socket, db).await;
         });
     }
 }
 
-async fn process(socket: TcpStream) {
+async fn process(socket: TcpStream, db: Db) {
     use mini_redis::Command::{self, Get, Set};
-    use std::collections::HashMap;
-
-    // a hashmap is used to store data
-    let mut db = HashMap::new();
 
     // `Connection`, provided by `mini-redis`, handles parsing frames from
     // the socket
@@ -32,16 +43,14 @@ async fn process(socket: TcpStream) {
     while let Some(frame) = connection.read_frame().await.unwrap() {
         let response = match Command::from_frame(frame).unwrap() {
             Set(cmd) => {
-                // the value is stored as `Vec<u8>`
-                db.insert(cmd.key().to_string(), cmd.value().to_vec());
+                let mut db = db.lock().unwrap();
+                db.insert(cmd.key().to_string(), cmd.value().clone());
                 Frame::Simple("OK".to_string())
             }
             Get(cmd) => {
+                let db = db.lock().unwrap();
                 if let Some(value) = db.get(cmd.key()) {
-                    // `Frame::Bulk` expects data to be of type `Bytes`
-                    // this type will be modified later
-                    // for now,`&Vec<u8>` is converted to `Bytes` using `into()`.
-                    Frame::Bulk(value.clone().into())
+                    Frame::Bulk(value.clone())
                 } else {
                     Frame::Null
                 }
