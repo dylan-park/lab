932d4e6771c1cfb67d12529e04f8390d41e8fd63
Dylan Park <dylan.park@att.net>
2026-04-15 22:11:56 -0500

    Initial 'hello world' for rust redis server

---
+++ rust/my-redis/src/main.rs
@@ -0,0 +1,15 @@
+use mini_redis::{Result, client};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // open a connection to mini-redis
+    let mut client = client::connect("127.0.0.1:6379").await?;
+    // set key "hello" with value "world"
+    client.set("hello", "world".into()).await?;
+    // get key "hello"
+    let result = client.get("hello").await?;
+
+    println!("got value from the server; result={:?}", result);
+
+    Ok(())
+}
